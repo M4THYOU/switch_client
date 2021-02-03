6e27412fd0b683d48d8156ff4db28a4f8ad9d358
Matt Wolfe <m5wolfe@uwaterloo.ca>
2021-02-03 09:54:48 -0500

    login routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
 // import logo from './logo.svg';
-// import {SignIn} from "./pages/SignIn";
+import {SignIn} from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
 
 // import { SwitchControl } from "./components/SwitchControl";
-
+// {/*<SwitchControl thingId="esp8266" />*/}
 function App() {
-  return (
-    <div className="App">
-        {/*<SwitchControl thingId="esp8266" />*/}
-        {/*<SignIn/>*/}
-        <SignUp/>
-    </div>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path='/register'>
+                    <SignUp />
+                </Route>
+                <Route path='/login'>
+                    <SignIn />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
